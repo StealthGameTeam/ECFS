60999b440589e2e954394f0be5b68edc9b598e48
Simon70 <simonstruck70@gmail.com>
2017-02-11 17:21:40 +0100

    Fuck

---
+++ girls.lua
@@ -0,0 +1,10 @@
+return {
+   wifi_waifu  =
+  {
+    name = " Wifi the router Waifu ",
+     talk  = {  like  = { min =  0 , max =  50  },   awkwardness  = { min =  0 , max =  50  },   coolness  = { min =  30 , max =  30  },   confidence  = { min =  0 , max =  10  }, },
+     interests  = { "internet", "porn sites", "finding shortest paths"},
+     date  = {  like  = { min =  50 , max =  60  },   awkwardness  = { min =  0 , max =  50  },   coolness  = { min =  30 , max =  30  },   confidence  = { min =  0 , max =  5  }, },
+     show  = {  awkwardness  = { min =  0 , max =  50  },   confidence  = { min =  0 , max =  10  }, },
+  },
+}
