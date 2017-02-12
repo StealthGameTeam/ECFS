0af5b8578b9661e6a94a4d5703abf136c2425370
nanderv <nanderv@gmail.com>
2017-02-12 13:32:13 +0100

    Removed test asset.

--- render/girls/draw_girl.lua
+++ render/girls/draw_girl.lua
@@ -1,5 +1,5 @@
 local bgs = {}
-bgs["wifi_waifu"] =  {img = love.graphics.newImage("assets/test/thomas.png"), x = 100, y = 400, r=0, sx = 0.5, sy = 0.5}
+--bgs["wifi_waifu"] =  {img = love.graphics.newImage("assets/test/thomas.png"), x = 100, y = 400, r=0, sx = 0.5, sy = 0.5}
 bgs["mom"] =  {img = love.graphics.newImage("assets/girls/phoneIMG.PNG"), x = 150, y = 150, r=0.1, sx = 0.8, sy = 0.8}
 
 return function(name)
